b437a0a7f4cffc725a97236280224aaa54b0da9c
lexgo <lexgolikoff@gmail.com>
2022-12-05 23:30:58 +0300

    some update from app.js

--- script/app.js
+++ script/app.js
@@ -3,14 +3,30 @@
 let quantityInCart = document.querySelector('.b-menu__quantityCart');
 // В переменную buttonsAddCart даем доступ к кнопкt Add to Cart карточек товаров. 
 let buttonsAddCart = document.querySelectorAll('.b-card__button');
-console.log(quantityInCart);
+// В переменную productName даем доступ к имени продукта.
+let productName = document.querySelector('.b-card__infoItem');
+// В переменную productPrice даем доступ к цене продукта.
+let productPrice = document.querySelector('.b-card__price');
 
 let count = 0;
 buttonsAddCart.forEach(element => {
-    element.addEventListener('click', function () {
+    element.addEventListener('click', function (event) {
         count = count + 1;
         quantityInCart.style.background = '#F16D7F';
         quantityInCart.innerText = count;
-
+        showProduct(event.target.setAttribute('id'));
+        console.log(event.target.dataset.type);
     });
 });
+
+function showProduct(product) {
+    let generatedText = '';
+    switch (product) {
+        case '01':
+
+    }
+}
+
+function getProductMarkup() {
+    return `<li>Product 01 <span>5шт.</span><span>52.00</span><span>260.00</span></li>`
+}
